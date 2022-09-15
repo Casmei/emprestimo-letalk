a09ad59a8b1d343ddd9129179c86277d42fe3e8f
Tiago de Castro Lima <casmei@protonmail.com>
2022-09-15 01:57:27 -0300

    config vite for view env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  envDir: './',
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 80
+  }
 })
